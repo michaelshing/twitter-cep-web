28e722661023f5f0f2d2a7f771d2aeccebe22e1a
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2013-06-14 18:25:41 +0900

    better looking with Bootstrap

--- src/main/java/org/example/TwitterCEPDelegate.java
+++ src/main/java/org/example/TwitterCEPDelegate.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.drools.ClockType;
@@ -13,6 +14,8 @@ import org.drools.agent.KnowledgeAgentFactory;
 import org.drools.agent.impl.PrintStreamSystemEventListener;
 import org.drools.base.evaluators.AfterEvaluatorDefinition;
 import org.drools.conf.EventProcessingOption;
+import org.drools.definition.KnowledgePackage;
+import org.drools.definition.rule.Rule;
 import org.drools.event.knowledgeagent.AfterChangeSetAppliedEvent;
 import org.drools.event.rule.DebugKnowledgeAgentEventListener;
 import org.drools.event.rule.DefaultKnowledgeAgentEventListener;
@@ -22,6 +25,8 @@ import org.drools.runtime.KnowledgeSessionConfiguration;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.conf.ClockTypeOption;
 import org.drools.runtime.rule.WorkingMemoryEntryPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import twitter4j.Status;
 import twitter4j.StatusListener;
@@ -29,32 +34,37 @@ import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 
 public class TwitterCEPDelegate {
-    
+
+    private static Logger logger = LoggerFactory.getLogger(TwitterCEPDelegate.class);
+
     static {
         // avoid classloading/serialization issue
         AfterEvaluatorDefinition after = new AfterEvaluatorDefinition();
     }
-    
+
     private static TwitterCEPDelegate singleton = new TwitterCEPDelegate();
 
     private static int MAX_RESPONSE_SIZE = 20;
 
     private KnowledgeAgent kagent;
     private StatefulKnowledgeSession ksession;
+
     private TwitterStream twitterStream;
     private List<Status> tweetList = new ArrayList<Status>();
+
+    private String ruleNamesInUse;
     private boolean kagentRunning = false;
     private boolean ksessionRunning = false;
-    
+
     private TwitterCEPDelegate() {
-        
+
     }
-    
+
     public static TwitterCEPDelegate getInstance() {
         return singleton;
     }
-    
-    public void run() {
+
+    public synchronized String run() {
 
         if (ksessionRunning) {
             throw new RuntimeException("ksession is running!");
@@ -63,12 +73,25 @@ public class TwitterCEPDelegate {
         // Creates a knowledge session
         ksession = createKnowledgeSession();
 
+        // store rule names in use
+        StringBuilder sb = new StringBuilder();
+        Collection<KnowledgePackage> knowledgePackages = ksession.getKnowledgeBase().getKnowledgePackages();
+        for (KnowledgePackage knowledgePackage : knowledgePackages) {
+            Collection<Rule> rules = knowledgePackage.getRules();
+            for (Rule rule : rules) {
+                sb.append(rule.getName() + ", ");
+            }
+        }
+        if (sb.length() > 2) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        ruleNamesInUse = sb.toString();
+
         tweetList = new ArrayList<Status>();
         ksession.setGlobal("tweetList", tweetList);
 
         // Gets the stream entry point
-        final WorkingMemoryEntryPoint ep = ksession
-                .getWorkingMemoryEntryPoint("twitter");
+        final WorkingMemoryEntryPoint ep = ksession.getWorkingMemoryEntryPoint("twitter");
 
         // Connects to the twitter stream and register the listener
         StatusListener listener = new TwitterStatusListener(ep);
@@ -85,38 +108,41 @@ public class TwitterCEPDelegate {
         }).start();
 
         ksessionRunning = true;
-        System.out.println("ksession is running");
+        logger.info("ksession is running");
+
+        return sb.toString();
     }
-    
+
     public String poll() {
-        System.out.println("poll tweets!");
+        logger.info("poll tweets!");
 
         StringBuffer buf = new StringBuffer();
-        int start = tweetList.size() < MAX_RESPONSE_SIZE ? 0 : tweetList.size()
-                - MAX_RESPONSE_SIZE;
-        List<Status> outList = new ArrayList<Status>(tweetList.subList(start,
-                tweetList.size()));
+
+        // display rules in use
+        buf.append("<div class='alert alert-success'><strong>rules = " + ruleNamesInUse + "</strong></div>");
+
+        int start = tweetList.size() < MAX_RESPONSE_SIZE ? 0 : tweetList.size() - MAX_RESPONSE_SIZE;
+        List<Status> outList = new ArrayList<Status>(tweetList.subList(start, tweetList.size()));
 
         for (int i = 0; i < outList.size(); i++) {
             Status status = outList.get(i);
-            buf.append("<span id='screenName'>"
-                    + status.getUser().getScreenName() + "</span> "
-                    + status.getText() + "<br>");
+            buf.append("<div class='alert alert-info'><strong>" + status.getUser().getScreenName() + "</strong> "
+                    + status.getText() + "</div>");
         }
 
         return buf.toString();
     }
 
-    public StatefulKnowledgeSession createKnowledgeSession() {
+    private StatefulKnowledgeSession createKnowledgeSession() {
         KnowledgeBase kbase = kagent.getKnowledgeBase();
-        System.out.println("createKnowledgeSession : kbase = " + kbase);
         KnowledgeSessionConfiguration ksconf = KnowledgeBaseFactory.newKnowledgeSessionConfiguration();
         ksconf.setOption(ClockTypeOption.get(ClockType.REALTIME_CLOCK.getId()));
         StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession(ksconf, null);
         return ksession;
     }
-    
-    public void stopKnowledgeSession() {
+
+    public synchronized void stopKnowledgeSession() {
+
         if (!ksessionRunning) {
             throw new RuntimeException("ksession is not running!");
         }
@@ -124,54 +150,58 @@ public class TwitterCEPDelegate {
         twitterStream.cleanUp();
         ksession.halt();
         ksession.dispose();
-        tweetList = new ArrayList<Status>();
 
         ksessionRunning = false;
-        System.out.println("ksession stopped");
+        logger.info("ksession stopped");
 
     }
 
-    public void startKnowledgeAgent() {
+    public synchronized void startKnowledgeAgent() {
 
-        if (kagent == null) {
-            // prepare kbase to convey kbase configuration
-            KnowledgeBaseConfiguration conf = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
-            conf.setOption(EventProcessingOption.STREAM);
-            KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase(conf);
-
-            // create kagent with change set
-            KnowledgeAgentConfiguration aconf = KnowledgeAgentFactory.newKnowledgeAgentConfiguration();
-            kagent = KnowledgeAgentFactory.newKnowledgeAgent("MyAgent", kbase, aconf);
-            kagent.addEventListener(new DebugKnowledgeAgentEventListener());
-            kagent.addEventListener(new HaltKnowledgeAgentEventListener());
-
-            kagent.applyChangeSet(ResourceFactory.newClassPathResource("ChangeSet.xml"));
-
-            // start ResourceChangeScanner
-            ResourceChangeScannerConfiguration sconf = ResourceFactory.getResourceChangeScannerService()
-                    .newResourceChangeScannerConfiguration();
-            sconf.setProperty("drools.resource.scanner.interval", "10");
-            ResourceFactory.getResourceChangeScannerService().configure(sconf);
-            ResourceFactory.getResourceChangeScannerService().setSystemEventListener(
-                    new PrintStreamSystemEventListener());
-            ResourceFactory.getResourceChangeScannerService().start();
-            ResourceFactory.getResourceChangeNotifierService().start();
-
-            kagentRunning = true;
-            System.out.println("kagent started!");
-        } else {
-            System.out.println("kagent is running!");
+        if (kagentRunning) {
+            throw new RuntimeException("kagent is running!");
         }
+
+        // prepare kbase to convey kbase configuration
+        KnowledgeBaseConfiguration conf = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
+        conf.setOption(EventProcessingOption.STREAM);
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase(conf);
+
+        // create kagent with change set
+        KnowledgeAgentConfiguration aconf = KnowledgeAgentFactory.newKnowledgeAgentConfiguration();
+        kagent = KnowledgeAgentFactory.newKnowledgeAgent("MyAgent", kbase, aconf);
+        kagent.addEventListener(new DebugKnowledgeAgentEventListener());
+        kagent.addEventListener(new HaltKnowledgeAgentEventListener());
+
+        kagent.applyChangeSet(ResourceFactory.newClassPathResource("ChangeSet.xml"));
+
+        // start ResourceChangeScanner
+        ResourceChangeScannerConfiguration sconf = ResourceFactory.getResourceChangeScannerService()
+                .newResourceChangeScannerConfiguration();
+        sconf.setProperty("drools.resource.scanner.interval", "10");
+        ResourceFactory.getResourceChangeScannerService().configure(sconf);
+        ResourceFactory.getResourceChangeScannerService().setSystemEventListener(new PrintStreamSystemEventListener());
+        ResourceFactory.getResourceChangeScannerService().start();
+        ResourceFactory.getResourceChangeNotifierService().start();
+
+        kagentRunning = true;
+        logger.info("kagent started!");
     }
-    
-    public void stopKnowledgeAgent() {
+
+    public synchronized void stopKnowledgeAgent() {
+
+        if (!kagentRunning) {
+            throw new RuntimeException("kagent is not running!");
+        }
 
         ResourceFactory.getResourceChangeScannerService().stop();
         ResourceFactory.getResourceChangeNotifierService().stop();
         kagent.monitorResourceChangeEvents(false);
+        kagent.dispose();
 
         kagentRunning = false;
         kagent = null;
+        logger.info("kagent stopped");
     }
 
     class HaltKnowledgeAgentEventListener extends DefaultKnowledgeAgentEventListener {
@@ -183,7 +213,7 @@ public class TwitterCEPDelegate {
                     singleton.run();
                 } catch (Exception e) {
                     // maybe fatal error
-                    e.printStackTrace();
+                    logger.error("restarting ksession failed", e);
                 }
             }
         }
--- src/main/java/org/example/TwitterCEPRest.java
+++ src/main/java/org/example/TwitterCEPRest.java
@@ -7,45 +7,39 @@ import javax.ws.rs.Produces;
 /**
  * 
  * REST facade of TwitterCEP
- *
+ * 
  */
 @Path("/")
 public class TwitterCEPRest {
-    
+
     private TwitterCEPDelegate delegate = TwitterCEPDelegate.getInstance(); // singleton
 
     @GET
     @Path("/start")
     @Produces({ "text/html" })
     public String start() {
-        delegate.startKnowledgeAgent();
-        delegate.run();
-        return "kagent and ksession is running";
+        try {
+            delegate.startKnowledgeAgent();
+            delegate.run();
+            return "<div class='alert alert-success'><strong>kagent and ksession is running</strong></div>";
+        } catch (Exception e) {
+            return "<div class='alert alert-error'><strong>" + e.toString() + "</strong></div>";
+        }
     }
-    
-    @GET
-    @Path("/refresh")
-    @Produces({ "text/html" })
-    public String refresh() {
-        delegate.stopKnowledgeSession();
-        delegate.run();
-        return "A new ksession is created and running";
-    }
-    
+
     @GET
     @Path("/stop")
     @Produces({ "text/html" })
     public String stop() {
-        StringBuilder sb = new StringBuilder();
-        String result = delegate.poll();
-        sb.append("kagent and ksession stopped<br>");
-        sb.append("-------------------------<br>");
-        sb.append(result);
-        delegate.stopKnowledgeSession();
-        delegate.stopKnowledgeAgent();
-        return sb.toString();
+        try {
+            delegate.stopKnowledgeSession();
+            delegate.stopKnowledgeAgent();
+            return "<div class='alert alert-success'><strong>kagent and ksession stopped</strong></div>";
+        } catch (Exception e) {
+            return "<div class='alert alert-error'><strong>" + e.toString() + "</strong></div>";
+        }
     }
-    
+
     @GET
     @Path("/poll")
     @Produces({ "text/html" })
